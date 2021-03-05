49c9e3c02e445ae98df6381cc1ff118bef5c56f9
Francesco Cerrone <france.cerrone@outlook.it>
2021-03-05 16:03:06 +0100

    attualwheter

--- ProgettoOBJWHE/src/main/java/com/example/ProgettoOBJWHE/parset/AttualWheater.java
+++ ProgettoOBJWHE/src/main/java/com/example/ProgettoOBJWHE/parset/AttualWheater.java
@@ -1,5 +1,11 @@
 package com.example.ProgettoOBJWHE.parset;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.*;
 
 
@@ -12,8 +18,8 @@ public class AttualWheater {
 	protected String city="Los Angeles";
 	private String date;
 	private double pression;
-	private double temp_max;
-	private double temp_min;
+	//private double temp_max;
+	//private double temp_min;
 	protected static String Apikey="aa0854721ceebf98625f7753971cd283";
 	private Long timeUNIX;
 	
@@ -37,7 +43,7 @@ public class AttualWheater {
 		this.pression = pression;
 	}
 
-	public double getTemp_max() {
+	/*public double getTemp_max() {
 		return temp_max;
 	}
 
@@ -51,7 +57,7 @@ public class AttualWheater {
 
 	public void setTemp_min(double temp_min) {
 		this.temp_min = temp_min;
-	}
+	}*/
 
 	public Long getTimeUNIX() {
 		return timeUNIX;
@@ -67,8 +73,34 @@ public class AttualWheater {
 
 	public static String getApikey() {
 		return Apikey;
-	};
+	}
 	
+	public void parsing() {}
+	
+	
+	
+	public void Setall(JSONObject obj) throws JSONException {
+		this.setTimeUNIX((Long) obj.get("dt"));
+		this.setDate(Dataformatter());
+		this.setPression((Double)Double.parseDouble(obj.getString("pressure").toString()));
+	}
+	 public JSONObject formatter() throws JSONException {
+		 JSONObject json=new JSONObject();
+		 json.put("city", this.city);
+		 json.put("timeUNIX", this.timeUNIX);
+		 json.put("date", this.date);
+		 json.put("pression", this.pression);
+		 
+		 return json;
+	 }
+	 
+	 public String Dataformatter() {
+		 Date data=new Date(this.timeUNIX*1000L);
+		 SimpleDateFormat formatdata=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		 formatdata.setTimeZone(TimeZone.getTimeZone("GMT"));
+		 String datadalUNIX=formatdata.format(data);
+		 return datadalUNIX;
+	 }
 	
 	
 	
